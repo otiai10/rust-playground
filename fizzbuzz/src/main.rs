820482430a4f6725ef54474323e296d9407ba299
otiai10 <otiai10@gmail.com>
2017-03-25 16:43:31 +0100

    FizzBuzz

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,7 +1,19 @@
 use std::env;
 
+fn fizzbuzz(x: i8) {
+    for i in 1..(x + 1) {
+        match (i % 3, i % 5) {
+            (0, 0) => println!("FizzBuzz!!!"),
+            (0, _) => println!("fizz"),
+            (_, 0) => println!("buzz"),
+            _ => println!("{}", i),
+        }
+    }
+}
+
 fn main() {
-    for argumens in env::args() {
-        println!("{}", argumens)
+    match env::args().last().unwrap().parse::<i8>() {
+        Ok(x) => fizzbuzz(x),
+        Err(err) => println!("数字を指定してください: {}", err),
     }
 }
